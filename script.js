3d864dc57e209d53f2816f6c31fadcd78f313dfb
ramkishore20 <ramkishoree1212@gmail.com>
2026-07-01 23:40:35 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+// //1) Even or Odd
+// const check = (num) => {
+//     if(num % 2 === 0){
+//         return "Even";
+        
+//     }
+//     else{
+//         return "Odd";
+        
+//     }
+// }
+
+// console.log(check(4));
+// console.log(check(7));
+// console.log(check(0));
+
+// // 2) Result Evaluator — Pass or Fail
+
+// function evaluateResult(num){
+//     if(num > 50 && num <=100){
+//         return "pass";
+        
+//     }
+//     else if(num<50 && num>=0){
+//         return "Fail";
+        
+//     }
+//     else{
+//         console.log("Invalid Marks");
+//         return null;
+//     }
+// }
+// console.log(evaluateResult(75));
+// console.log(evaluateResult(40));
+// console.log(evaluateResult(120));
+
+// // 3) Maximum Finder — Largest of Three Numbers
+
+// const findMax = (num1,num2,num3) => {
+//     if(num1 >= num2 && num1 >= num3){
+//         return num1; 
+//     }
+//     else if(num2 >= num1 && num2 >= num3){
+//         return num2;
+//     }
+//     else{
+//         return num3;
+//     }
+// }
+
+// console.log(findMax(10,25,15));
+// console.log(findMax(5,3,8));
+// console.log(findMax(9,9,2));
+
+
